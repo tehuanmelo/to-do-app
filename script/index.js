df741a2e6ac34d8911c54510bdc8e6ca458bf462
tehuanmelo <tehuanmelo@gmail.com>
2025-02-03 13:43:29 +0400

    (fix) adding empty task

--- script/index.js
+++ script/index.js
@@ -9,9 +9,15 @@ todoForm.addEventListener('submit', event => {
 
 const addTodo = () => {
     const todoText = todoInput.value.trim();
-    todoArray.push(todoText);
-    saveTodos();
-    renderList();
+    const todoObject = {
+        text: todoText,
+        completed: false
+    }
+    if (todoText) {
+        todoArray.push(todoObject);
+        saveTodos();
+        renderList();
+    }
     todoInput.value = '';
 }
 
@@ -27,12 +33,12 @@ const createListItem = (todo, todoIndex) => {
     const listItem = document.createElement('li');
     listItem.className = 'to-do'
     listItem.innerHTML = `
-        <input id="to-do-${todoIndex}" type="checkbox">
+        <input id="to-do-${todoIndex}" type="checkbox" ${todo.completed ? 'checked': ''}>
         <label for="to-do-${todoIndex}" class="custom-checkbox">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
         </label>
         <label for="to-do-${todoIndex}" class="to-do-text">
-            ${todo}
+            ${todo.text}
         </label>
         <button class="delete-button">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
@@ -41,6 +47,11 @@ const createListItem = (todo, todoIndex) => {
     const deleteButton = listItem.querySelector('.delete-button')
     deleteButton.addEventListener('click', () => {
         deleteTodo(todoIndex)
+    })
+    const checkbox = listItem.querySelector('input');
+    checkbox.addEventListener('change', () => {
+        todoArray[todoIndex].completed = checkbox.checked
+        saveTodos();
     })
     return listItem;
 }
